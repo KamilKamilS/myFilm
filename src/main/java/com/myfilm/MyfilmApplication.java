38d756982cb8cf900c33bd1e7eb7371669a6af96
Kamil <s.kamilx@gmail.com>
2021-01-31 23:30:39 +0100

    Init project

---
+++ src/main/java/com/myfilm/MyfilmApplication.java
@@ -0,0 +1,13 @@
+package com.myfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfilmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfilmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myfilm/MyfilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
