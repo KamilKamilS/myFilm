ce6389da54c27ea56bae2e46737af1dff8077a47
Kamil <s.kamilx@gmail.com>
2021-02-05 23:49:08 +0100

    Add findMovieByTitle method to MovieRepository

--- src/main/java/com/myfilm/repository/MovieRepository.java
+++ src/main/java/com/myfilm/repository/MovieRepository.java
@@ -3,5 +3,9 @@ package com.myfilm.repository;
 import com.myfilm.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    public Optional<Movie> findMovieByTitle(String title);
 }
