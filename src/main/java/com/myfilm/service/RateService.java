e8a2cc78c7b2508570d3e50da25170cb52f0aea3
Kamil <s.kamilx@gmail.com>
2021-02-15 23:56:13 +0100

    Remove unnecessary comment in RateService

--- src/main/java/com/myfilm/service/RateService.java
+++ src/main/java/com/myfilm/service/RateService.java
@@ -24,7 +24,6 @@ public class RateService {
         if (movieInCatalogue.isEmpty()) {
             throw new MovieNotFoundInCatalogueException(" ");
         } else {
-//            Movie movieFromCatalogue = movieInCatalogue.get();
             List<Rate> rateList = movieInCatalogue.get().getRates();
             rateList.add(rate);
             movieInCatalogue.get().setRates(rateList);
