811bd428c23bc356c724a3efe93826badc6e8f9f
Kamil <s.kamilx@gmail.com>
2021-02-18 23:57:17 +0100

    Add methods to SessionCartService

--- src/main/java/com/myfilm/service/SessionCartService.java
+++ src/main/java/com/myfilm/service/SessionCartService.java
@@ -1,12 +1,59 @@
 package com.myfilm.service;
 
+import com.myfilm.model.Movie;
+import com.myfilm.repository.CartService;
+import com.myfilm.repository.MovieRepository;
+import com.myfilm.service.exception.MovieNotFoundInCatalogueException;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Map;
+
 @Service
 @Scope(value = WebApplicationContext.SCOPE_SESSION,
         proxyMode = ScopedProxyMode.TARGET_CLASS)
-public class SessionCartService {
+public class SessionCartService implements CartService {
+
+    private Map<Movie, Integer> movies;
+    private Map<Movie, Integer> unavailableMovies;
+    private MovieRepository movieRepository;
+
+    @Override
+    public void addMovieToCart(Movie movie) throws MovieNotFoundInCatalogueException {
+        if (movieRepository.findMovieByTitle(movie.getTitle()).isEmpty()) {
+            throw new MovieNotFoundInCatalogueException(movie.getTitle());
+        } else {
+            movies.put(movie, this.getQuantities(movie));
+        }
+    }
+
+    public Movie removeOneCopyOfMovieFromCart(Movie movie) throws MovieNotFoundInSessionCart {
+        if (movies.containsKey(movie)) {
+            removeOneCopy(movie);
+            return movie;
+        } else {
+            throw new MovieNotFoundInSessionCart(movie.getTitle());
+        }
+    }
+
+    private void removeOneCopy(Movie movie) {
+        int numberOfCopies = movies.get(movie);
+        if (numberOfCopies > 1) {
+            movies.replace(movie, --numberOfCopies);
+        } else {
+            movies.remove(movie);
+        }
+    }
+
+    @Override
+    public Map<Movie, Integer> getSessionCart() {
+        return null;
+    }
+
+    private int getQuantities(Movie movie) {
+        return getMovies().get(movie) == null ? 1 : getMovies().get(movie) + 1;
+    }
+
 }
